4b2d5d6ac86aafd6c0094f32ef51a2d59c2100da
nmadhab <nilmadhab1994@booking.com>
2021-04-19 08:01:05 +0530

    postgres working

--- src/main/java/com/simplecoding/social/config/DatabaseConfig.java
+++ src/main/java/com/simplecoding/social/config/DatabaseConfig.java
@@ -3,17 +3,16 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 import javax.sql.DataSource;
 
-@Configuration
+//@Configuration
 public class DatabaseConfig {
-
-  @Value("${spring.datasource.url}")
-  //@Value("jdbc:postgresql://localhost:5432/social_network")
-  private String dbUrl;
-
-  @Bean
-  public DataSource dataSource() {
-      HikariConfig config = new HikariConfig();
-      config.setJdbcUrl(dbUrl);
-      return new HikariDataSource(config);
-  }
+    //heroku create@Value("${spring.datasource.url}")
+//  @Value("jdbc:postgresql://localhost:5432/social_network")
+//  private String dbUrl;
+//
+//  @Bean
+//  public DataSource dataSource() {
+//      HikariConfig config = new HikariConfig();
+//      config.setJdbcUrl(dbUrl);
+//      return new HikariDataSource(config);
+//  }
 }
